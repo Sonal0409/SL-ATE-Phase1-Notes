a21846f771fce72b86f4b7d12d24a5fe1d871e9e
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2023-09-21 22:33:37 +0530

    Create ExplicitTypecasting.java

---
+++ src/ExplicitTypecasting.java
@@ -0,0 +1,27 @@
+package javaPrograms;
+
+public class ExplicitTypecasting {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Explicit typecasting demo
+		
+		// int size is just 4 bytes, double size is 8 bytes
+		
+		double a=11.4;
+		
+		
+		 // int b;
+		
+		// Convert a variable which is a double into integer
+		
+		int b = (int)a ; // value of double a is now has datatype int
+		
+		System.out.println(b);
+		
+		
+		
+	}
+
+}

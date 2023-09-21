f3c270f741f0246e0a436e729f9fb5a91581730c
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2023-09-21 22:33:01 +0530

    Create ImplicitTypeCasting.java

---
+++ src/ImplicitTypeCasting.java
@@ -0,0 +1,26 @@
+package javaPrograms;
+
+public class ImplicitTypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// implicit typecasting demo
+		
+		// int size is just 4 bytes, double size is 8 bytes
+		
+		int a=10;
+		
+		 // double b;
+		
+		// Convert a which is an integer into double
+		
+		double b = a ; // value of int a is now has datatype double
+		
+		System.out.println(b);
+		
+		
+		
+	}
+
+}
